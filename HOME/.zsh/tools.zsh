c0d1febae1f2f56c82ab5d5a6ca1ec55015422a0
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-16 22:08:02 -0600

    zsh: remove code

--- HOME/.zsh/tools.zsh
+++ HOME/.zsh/tools.zsh
@@ -1,69 +1,39 @@
+#!/usr/bin/env zsh
 # External Zsh tools
 
-# iTerm2
-test -e "${HOME}/.iterm2_shell_integration.zsh" && source "${HOME}/.iterm2_shell_integration.zsh"
+## homebrew
+eval $(/opt/homebrew/bin/brew shellenv)
 
-# GPG (GNU Privacy Guard or GnuPG)
-export GPG_TTY=$(tty)
+[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+## broot
+source /Users/christopherbocardo/.config/broot/launcher/bash/br
 
 ## zsh-completions
-fpath=(/usr/local/share/zsh-completions $fpath) # activate zsh-completions
-fpath=($HOME/.zsh/zsh-completions $fpath)       # activate zsh-completions
+FPATH=$(brew --prefix)/share/zsh-completions:$FPATH
+fpath=($HOME/.zsh/zsh-completions $fpath)
 
-# broot
-source $HOME/.config/broot/launcher/bash/br
-
-# zsh-autosuggestions
+## zsh-autosuggestions
 source /opt/homebrew/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=213"
 
-# zsh-syntax-highlighting
+## zsh-syntax-highlighting
+source /opt/homebrew/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 export ZSH_HIGHLIGHT_HIGHLIGHTERS_DIR=/opt/homebrew/share/zsh-syntax-highlighting/highlighters
-
 source $(brew --prefix zsh-syntax-highlighting)/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 
 # show when running in a shell that was spawned by ranger
 if [ -n "$RANGER_LEVEL" ]; then export PS1="[ranger]$PS1"; fi
 
-# starship
+## starship
 eval "$(starship init zsh)"
+
 # set window title via starship
 function set_win_title(){
     echo -ne "\033]0; $(realpath .) \007"
 }
+
 precmd_functions+=(set_win_title)
-
-# homebrew
-export PATH="/opt/homebrew/opt/curl/bin:$PATH"
-eval "$(/opt/homebrew/bin/brew shellenv)"
-
-# nodenv
-eval "$(nodenv init -)"
-
-# pnpm
-export PNPM_HOME="$HOME/Library/pnpm"
-export PATH="$PNPM_HOME:$PATH"
 
 # set Ripgrep configuration file
 export RIPGREP_CONFIG_PATH="$HOME/.config/ripgreprc"
-
-## ranger
-# show when running in a shell that was spawned by ranger
-if [ -n "$RANGER_LEVEL" ]; then export PS1="[ranger]$PS1"; fi
-
-# C-g to quit and cd into directory
-function ranger {
-    local IFS=$'\t\n'
-    local tempfile="$(mktemp -t tmp.XXXXXX)"
-    local ranger_cmd=(
-        command
-        ranger
-        --cmd="map <c-g> chain shell echo %d > "$tempfile"; quitall"
-    )
-
-    ${ranger_cmd[@]} "$@"
-    if [[ -f "$tempfile" ]] && [[ "$(cat -- "$tempfile")" != "$(echo -n `pwd`)" ]]; then
-        cd -- "$(cat "$tempfile")" || return
-    fi
-    command rm -f -- "$tempfile" 2>/dev/null
-}
