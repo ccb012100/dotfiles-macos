b513b7bce5e910c4f268b70e6bacf9a3c9ae1acb
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2022-12-16 22:08:01 -0600

    zsh: add hashbang to scripts

--- HOME/.zsh/completions.zsh
+++ HOME/.zsh/completions.zsh
@@ -1,4 +1,4 @@
-#!/usr/bin/zsh
+#!/usr/bin/env zsh
 # Zsh auto-completion
 
 autoload -U compinit && compinit    # zsh-completions
--- HOME/.zsh/fzf_config.zsh
+++ HOME/.zsh/fzf_config.zsh
@@ -1,21 +1,18 @@
+#!/usr/bin/env zsh
 # fzf
 # https://github.com/junegunn/fzf
 
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
-
-export FZF_COMPLETION_TRIGGER=';;'  # default is '**'
-bindkey '^i' fzf-completion         # also binds <TAB> key
+export FZF_COMPLETION_TRIGGER=';;' # default is '**'
 
 # use ripgrep instead of find
 export FZF_DEFAULT_COMMAND="rg --files --hidden --follow --glob '!.git' --exclude node_modules"
-export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_COMPLETION_OPTS='--border --info=inline'
-#color theme: tomasr/molokai
+# color theme: tomasr/molokai
 export FZF_DEFAULT_OPTS="--height=75% --cycle --multi --info=inline --border \
 --tabstop=4 --header-lines=1 \
 --bind='?:toggle-preview,f12:execute(less -f {})' \
 --header=$'[ (?) Toggle preview ][ (F12) pipe to less ]' \
---preview-window=:hidden --preview 'batcat --style=numbers --color=always {}' \
+--preview-window=:hidden --preview 'bat --style=numbers --color=always {}'
 --color=bg+:#293739,bg:#1B1D1E,border:#808080,spinner:#E6DB74,hl:#7E8E91,fg:#F8F8F2,header:#7E8E91,info:#A6E22E,pointer:#A6E22E,marker:#F92672,fg+:#F8F8F2,prompt:#F92672,hl+:#F92672"
 
 # Use fd (https://github.com/sharkdp/fd) instead of the default find
@@ -34,7 +31,10 @@ _fzf_compgen_dir() {
 # find-in-file - usage: fif <searchTerm>
 # from https://github.com/junegunn/fzf/wiki/examples
 fif() {
-  if [ ! "$#" -gt 0 ]; then echo "Need a string to search for!"; return 1; fi
+  if [ ! "$#" -gt 0 ]; then
+    echo "Need a string to search for!"
+    return 1
+  fi
   rg --files-with-matches --no-messages "$1" | fzf --preview "highlight -O ansi -l {} 2> /dev/null | rg --colors 'match:bg:yellow' --ignore-case --pretty --context 10 '$1' || rg --ignore-case --pretty --context 10 '$1' {}"
 }
 
