ed71d8aff400ba25a0dff36565d1e61ef9018e3c
Christopher Bocardo <397636+ccb012100@users.noreply.github.com>
2023-03-05 09:42:49 -0600

    refactor rsync file to use --files-from

--- rsync-to-git.sh
+++ rsync-to-git.sh
@@ -9,8 +9,6 @@ fi
 
 gitdir="$HOME/.dotfiles"
 
-# add -n,--dry-run flag if testing
-
 sync_file() {
     rsync --recursive --times --progress --protect-args "$1" "$gitdir/root/$2"
 }
@@ -19,104 +17,6 @@ sync_from_home() {
     rsync --recursive --times --progress --protect-args "$HOME/$1" "$gitdir/HOME/$2"
 }
 
-#sync_file \
-#    "/Library/LaunchDaemons/local.kmonad.plist" \
-#    "Library/LaunchDaemons/"
-
-sync_from_home \
-    "Library/Application Support/Alfred/Alfred.alfredpreferences/preferences" \
-    "Library/Application Support/Alfred/Alfred.alfredpreferences/"
-
-sync_from_home \
-    "Library/Application Support/qmk/qmk.ini" \
-    "Library/Application Support/qmk/"
-
-sync_from_home \
-    "Library/Application Support/Code/User/keybindings.json" \
-    "Library/Application Support/Code/User/"
-
-sync_from_home Library/Application\ Support/sioyek/{prefs,keys}_user.config \
-    "Library/Application Support/sioyek/"
-
-sync_from_home "Library/Application Support/sioyek/keys_user.config" \
-    "Library/Application Support/sioyek/"
-
-sync_from_home \
-    ".zshrc" \
-    ""
-
-sync_from_home \
-    ".local/NOTES.code-workspace" \
-    ".local/"
-
-sync_from_home \
-    ".gitconfig" \
-    ""
-
-sync_from_home \
-    ".hammerspoon/init.lua" \
-    ".hammerspoon/"
-
-sync_from_home \
-    ".config/bat/bat.conf" \
-    ".config/bat/"
-
-sync_from_home \
-    ".config/broot/conf.hjson" \
-    ".config/broot/"
-
-sync_from_home \
-    ".config/gh/config.yml" \
-    ".config/gh/"
-
-sync_from_home \
-    ".config/git" \
-    ".config/"
-
-sync_from_home \
-    ".config/karabiner/karabiner.json" \
-    ".config/karabiner/"
-
-sync_from_home \
-    ".config/karabiner/assets" \
-    ".config/karabiner/"
-
-sync_from_home \
-    ".config/kitty" \
-    ".config/"
-
-sync_from_home \
-    ".config/micro/settings.json" \
-    ".config/micro/"
-
-sync_from_home \
-    ".config/neofetch/config.conf" \
-    ".config/neofetch/"
-
-sync_from_home \
-    ".config/pgcli/config" \
-    ".config/pgcli/"
-
-sync_from_home \
-    ".config/ranger/rc.conf" \
-    ".config/ranger/"
-
-sync_from_home \
-    ".config/ripgreprc" \
-    ".config/"
-
-sync_from_home \
-    ".config/starship.toml" \
-    ".config/"
-
-sync_from_home \
-    ".config/warpd/config" \
-    ".config/warpd/"
-
-sync_from_home \
-    ".vim/vimrc" \
-    ".vim/"
-
-rsync --recursive --times --progress --protect-args \
-    ~/.zsh/{aliases,completions,funcs,fzf_config,tools,unalias}.zsh \
-    "$gitdir/HOME/.zsh/"
+# --verbose --dry-run
+rsync --dirs --recursive --times --progress \
+    --files-from=files-to-sync "$HOME" "$gitdir/HOME"
